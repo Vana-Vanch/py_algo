2c03aa33bf13fd41d41a3e4c5e214e3afb542213
Vana-Vanch <vanavanch98@gmail.com>
2021-06-26 21:04:47 -0700

    yahoo

--- check_array_is_sorted.py
+++ check_array_is_sorted.py
@@ -1,8 +1,6 @@
 def isArraySorted(arr):
-    if len(arr) == 0 or len(arr) == 1:
-        return True
-    if arr[0] > arr[1]:
-        return False
+    if len(arr) == 0 or len(arr) == 1:return True
+    if arr[0] > arr[1]:return False
     return isArraySorted(arr[1:])    
 
 arr = [1,2,3,4,5,6,7,8,9,10]
